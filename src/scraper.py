f42ab5ed16a7195d5665f30c9ff82f0826dee7e4
Talha <talhayenihayat54@gmail.com>
2025-10-23 22:46:56 +0200

    adds header

--- src/scraper.py
+++ src/scraper.py
@@ -1,2 +1,13 @@
-import sys
-print(sys.executable)
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.5'
+}
+
+def get_product_details(product_url: str) -> dict:
+    product_details = {}
+
+    page = requests.get(product_url, headers=headers)
+    soup = BeautifulSoup(page.content, features='lxml')
